b29183d68547e7ca3b4def51aa1489f31a11f635
Sorah Fukumori <her@sorah.jp>
2022-08-23 06:36:34 +0900

    aws_route53_zone.ptr-10 => 10.in-addr.arpa.

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -15,6 +15,19 @@ resource "aws_route53_zone_association" "rubykaigi-net_main" {
   vpc_id  = aws_vpc.main.id
 }
 
+resource "aws_route53_zone" "ptr-10" {
+  name = "10.in-addr.arpa"
+
+  lifecycle {
+    ignore_changes = [vpc]
+  }
+}
+
+resource "aws_route53_zone_association" "ptr-10_main" {
+  zone_id = aws_route53_zone.ptr-10.zone_id
+  vpc_id  = aws_vpc.main.id
+}
+
 locals {
   rubykaigi_net_zones = toset([aws_route53_zone.rubykaigi-net_public.zone_id, aws_route53_zone.rubykaigi-net_private.zone_id])
 }
