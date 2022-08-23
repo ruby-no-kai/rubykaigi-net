dd6967e13a1d777847b45302c66398e9235c3ac0
Sorah Fukumori <her@sorah.jp>
2022-08-23 12:35:42 +0900

    provision node groups

--- tf/k8s/vpc.tf
+++ tf/k8s/vpc.tf
@@ -61,3 +61,33 @@ data "aws_subnet" "main-private-d" {
     values = ["private"]
   }
 }
+
+data "aws_subnet" "main-onpremises-c" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main.id]
+  }
+  filter {
+    name   = "availability-zone"
+    values = ["ap-northeast-1c"]
+  }
+  filter {
+    name   = "tag:Tier"
+    values = ["onpremises"]
+  }
+}
+
+data "aws_subnet" "main-onpremises-d" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main.id]
+  }
+  filter {
+    name   = "availability-zone"
+    values = ["ap-northeast-1d"]
+  }
+  filter {
+    name   = "tag:Tier"
+    values = ["onpremises"]
+  }
+}
