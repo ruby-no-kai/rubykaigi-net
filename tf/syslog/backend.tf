71ae2b9d8bd6ea6e3dad605193f153b35a7ab215
Kasumi Hanazuki <kasumi@rollingapple.net>
2022-09-01 08:49:26 +0000

    tf/syslog: k8s service account for fluentd

--- tf/syslog/backend.tf
+++ tf/syslog/backend.tf
@@ -6,3 +6,12 @@ terraform {
     dynamodb_table = "rk-terraform"
   }
 }
+
+data "terraform_remote_state" "k8s" {
+  backend = "s3"
+  config = {
+    bucket = "rk-infra"
+    region = "ap-northeast-1"
+    key    = "terraform/nw-k8s.tfstate"
+  }
+}
