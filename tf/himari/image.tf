4f2bf19aabc78bde055859280f60340d7b6f3066
Sorah Fukumori <her@sorah.jp>
2023-03-22 08:59:58 +0900

    himari: roll sorah/himari@8c729831affc6f191766b7cda782636135540c60

--- tf/himari/image.tf
+++ tf/himari/image.tf
@@ -3,5 +3,5 @@ module "himari_image" {
   source = "github.com/sorah/himari//himari-aws/lambda/terraform/image"
 
   repository_name  = "himari-lambda"
-  source_image_tag = "fabd90eaca33dd21d8b376ffe17f8a461e3fe9ae"
+  source_image_tag = "8c729831affc6f191766b7cda782636135540c60"
 }
