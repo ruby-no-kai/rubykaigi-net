cdd37fd00387f815114a2cbf093e9a710d96e6dc
Sorah Fukumori <her@sorah.jp>
2023-04-03 23:15:13 +0900

    update hosts and use roadworker

--- tf/dns-hosts/aws.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  region              = "ap-northeast-1"
-  allowed_account_ids = ["005216166247"]
-}
-
-data "aws_caller_identity" "current" {}
--- tf/dns-hosts/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "rk-infra"
-    region = "ap-northeast-1"
-    key    = "terraform/dns-hosts.tfstate"
-    dynamodb_table = "rk-terraform"
-  }
-}
