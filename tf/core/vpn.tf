b524c5ad888c74c2d295e8093006b3619d5ae8b4
Sorah Fukumori <her@sorah.jp>
2023-04-05 01:30:48 +0900

    core: Add vpn connection to br-01.nrt
    https://github.com/ruby-no-kai/rubykaigi-nw/issues/29

---
+++ tf/core/vpn.tf
@@ -0,0 +1,25 @@
+resource "aws_vpn_connection" "rk23-nrt" {
+  vpn_gateway_id      = aws_vpn_gateway.main.id
+  customer_gateway_id = aws_customer_gateway.rk23-nrt.id
+  type                = aws_customer_gateway.rk23-nrt.type
+  static_routes_only  = false
+
+  tunnel1_ike_versions = ["ikev2"]
+  tunnel2_ike_versions = ["ikev2"]
+
+  tags = {
+    Name    = "rk23-nrt"
+    Project = "rk23net"
+  }
+}
+
+resource "aws_customer_gateway" "rk23-nrt" {
+  bgp_asn    = 65002
+  ip_address = "117.102.186.171"
+  type       = "ipsec.1"
+
+  tags = {
+    Name    = "rk23-nrt"
+    Project = "rk23net"
+  }
+}
