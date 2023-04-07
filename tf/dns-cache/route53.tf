4867380c57c3c99afba2e0219e8a2b263ccfc597
Kasumi Hanazuki <kasumi@rollingapple.net>
2023-04-07 07:11:45 +0000

    dns: Manage resolver.rubykaigi.net CAA with Roadworker

--- tf/dns-cache/route53.tf
+++ tf/dns-cache/route53.tf
@@ -19,15 +19,3 @@ resource "aws_route53_record" "do53" {
     aws_lb.nlb.dns_name,
   ]
 }
-resource "aws_route53_record" "caa" {
-  for_each = local.rubykaigi_net_zones
-  zone_id  = each.value
-  name     = "resolver.rubykaigi.net"
-  type     = "CAA"
-  ttl      = 300
-  records = [
-    "0 issue \"amazonaws.com\"",
-    "0 issue \"letsencrypt.org\"",
-    "0 issue \"sectigo.com\"", # ZeroSSL
-  ]
-}
