b60cc6dd9565be84dd9127024d0e78c13d521a66
Kasumi Hanazuki <kasumi@rollingapple.net>
2023-04-11 16:59:57 +0000

    tf/ops-lb: Remove unused provider

--- tf/ops-lb/aws.tf
+++ tf/ops-lb/aws.tf
@@ -3,10 +3,5 @@ provider "aws" {
   region              = "ap-northeast-1"
   allowed_account_ids = ["005216166247"]
 }
-provider "aws" {
-  alias               = "use1"
-  region              = "us-east-1"
-  allowed_account_ids = ["005216166247"]
-}
 
 data "aws_caller_identity" "current" {}
