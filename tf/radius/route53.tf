41e3abf1716493bebbc5a967005209d26772746e
Sorah Fukumori <her@sorah.jp>
2023-04-16 00:40:06 +0900

    radius: route53 radius.rk.n

---
+++ tf/radius/route53.tf
@@ -0,0 +1,21 @@
+data "aws_route53_zone" "rubykaigi_net-public" {
+  name         = "rubykaigi.net."
+  private_zone = false
+}
+data "aws_route53_zone" "rubykaigi_net-private" {
+  name         = "rubykaigi.net."
+  private_zone = true
+}
+locals {
+  rubykaigi_net_zones = toset([data.aws_route53_zone.rubykaigi_net-public.zone_id, data.aws_route53_zone.rubykaigi_net-private.zone_id])
+}
+
+resource "aws_route53_record" "radius" {
+  zone_id = data.aws_route53_zone.rubykaigi_net-private.zone_id
+  name    = "radius.rubykaigi.net"
+  type    = "CNAME"
+  ttl     = 300
+  records = [
+    aws_lb.nlb.dns_name,
+  ]
+}
