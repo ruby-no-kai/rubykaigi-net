719003bb4e6a3ad2f0fd897b656938d0683f35f4
Sorah Fukumori <her@sorah.jp>
2023-04-15 22:04:17 +0900

    radiusd: enable status server

--- docker/radiusd/run.sh
+++ docker/radiusd/run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -xe
+
 RADIUS_SECRET=${RADIUS_SECRET:-testing123}
 sed -i -e "s/__RADIUS_SECRET__/${RADIUS_SECRET}/g" /etc/freeradius/3.0/clients.conf
+
 if [[ "${RADIUS_DEBUG}" = "1" ]];then
   isdebug=-xx
 else
   isdebug=
 fi
+
 exec /usr/sbin/freeradius $isdebug -f -lstdout
