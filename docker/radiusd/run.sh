98077534baad39d862da6e252979191152d6f6c6
Sorah Fukumori <her@sorah.jp>
2023-04-16 00:23:14 +0900

    radiusd: prevent RADIUS_SECRET to appear in stdout

--- docker/radiusd/run.sh
+++ docker/radiusd/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 RADIUS_SECRET=${RADIUS_SECRET:-testing123}
 sed -i -e "s/__RADIUS_SECRET__/${RADIUS_SECRET}/g" /etc/freeradius/3.0/clients.conf
@@ -9,4 +9,5 @@ else
   isdebug=
 fi
 
+set -x
 exec /usr/sbin/freeradius $isdebug -f -lstdout
