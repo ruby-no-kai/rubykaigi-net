400edb4d94e062254a29c844ac0b0c91aa0e365d
Sorah Fukumori <her@sorah.jp>
2023-06-29 19:23:21 +0900

    github_actions_iam: update ca thumbprint_list
    https://github.blog/changelog/2023-06-27-github-actions-update-on-oidc-integration-with-aws/

--- tf/core/github_actions_iam.tf
+++ tf/core/github_actions_iam.tf
@@ -6,7 +6,7 @@ resource "aws_iam_openid_connect_provider" "github-actions" {
   ]
 
   thumbprint_list = [
-    "a031c46782e6e6c662c2c87c76da9aa62ccabd8e",
     "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
   ]
 }
