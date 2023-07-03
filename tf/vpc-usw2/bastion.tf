7b061077934858b6626c63cd87516fbc7fa31dcf
Sorah Fukumori <her@sorah.jp>
2023-07-04 04:04:20 +0900

    terminate usw2 bastion and apne1/nrt vpn for rk23

--- tf/vpc-usw2/bastion.tf
+++ tf/vpc-usw2/bastion.tf
@@ -14,9 +14,9 @@ resource "aws_security_group_rule" "bastion_ssh9922" {
   ipv6_cidr_blocks  = ["::/0"]
 }
 
-resource "aws_eip" "bastion" {
-  vpc = true
-}
+#resource "aws_eip" "bastion" {
+#  vpc = true
+#}
 
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -31,48 +31,48 @@ data "aws_iam_instance_profile" "bastion" {
   name = "NwBastion"
 }
 
-resource "aws_instance" "bastion" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t4g.nano"
-  subnet_id     = aws_subnet.c_public.id
-
-  vpc_security_group_ids = [aws_security_group.default.id, aws_security_group.bastion.id]
-  iam_instance_profile   = data.aws_iam_instance_profile.bastion.name
-  #key_name               = data.aws_key_pair.default.key_name
-
-  user_data = file("./bastion.yml")
-
-  tags = {
-    Name = "bastion"
-  }
-  lifecycle {
-    ignore_changes = [ami]
-  }
-}
-
-resource "aws_eip_association" "bastion" {
-  instance_id   = aws_instance.bastion.id
-  allocation_id = aws_eip.bastion.id
-}
-
-resource "aws_route53_record" "aaaa-bastion-usw2_rubykaigi_net" {
-  for_each = local.rubykaigi_net_zones
-  name     = "bastion-usw2.rubykaigi.net."
-  zone_id  = each.value
-  type     = "AAAA"
-  ttl      = 60
-  records = [
-    aws_instance.bastion.ipv6_addresses[0],
-  ]
-}
-resource "aws_route53_record" "bastion-usw2_rubykaigi_net" {
-  for_each = local.rubykaigi_net_zones
-  name     = "bastion-usw2.rubykaigi.net."
-  zone_id  = each.value
-  type     = "A"
-  ttl      = 60
-  records = [
-    aws_eip.bastion.public_ip,
-  ]
-}
+#resource "aws_instance" "bastion" {
+#  ami           = data.aws_ami.ubuntu.id
+#  instance_type = "t4g.nano"
+#  subnet_id     = aws_subnet.c_public.id
+#
+#  vpc_security_group_ids = [aws_security_group.default.id, aws_security_group.bastion.id]
+#  iam_instance_profile   = data.aws_iam_instance_profile.bastion.name
+#  #key_name               = data.aws_key_pair.default.key_name
+#
+#  user_data = file("./bastion.yml")
+#
+#  tags = {
+#    Name = "bastion"
+#  }
+#  lifecycle {
+#    ignore_changes = [ami]
+#  }
+#}
+#
+#resource "aws_eip_association" "bastion" {
+#  instance_id   = aws_instance.bastion.id
+#  allocation_id = aws_eip.bastion.id
+#}
+#
+#resource "aws_route53_record" "aaaa-bastion-usw2_rubykaigi_net" {
+#  for_each = local.rubykaigi_net_zones
+#  name     = "bastion-usw2.rubykaigi.net."
+#  zone_id  = each.value
+#  type     = "AAAA"
+#  ttl      = 60
+#  records = [
+#    aws_instance.bastion.ipv6_addresses[0],
+#  ]
+#}
+#resource "aws_route53_record" "bastion-usw2_rubykaigi_net" {
+#  for_each = local.rubykaigi_net_zones
+#  name     = "bastion-usw2.rubykaigi.net."
+#  zone_id  = each.value
+#  type     = "A"
+#  ttl      = 60
+#  records = [
+#    aws_eip.bastion.public_ip,
+#  ]
+#}
 
