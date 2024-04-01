d36fa2b7755068fc5bc88b4a7f85db154f10f0fb
Sorah Fukumori <her@sorah.jp>
2024-04-02 06:09:34 +0900

    vpn: change asn

--- tf/core/vpn.tf
+++ tf/core/vpn.tf
@@ -1,3 +1,16 @@
+locals {
+  vpn_ikev2_options = {
+    phase1_dh_group_numbers      = [14]
+    phase1_encryption_algorithms = ["AES256-GCM-16"]
+    phase1_integrity_algorithms  = ["SHA2-512"]
+    phase1_lifetime_seconds      = 21600
+    phase2_dh_group_numbers      = [14]
+    phase2_encryption_algorithms = ["AES256"]
+    phase2_integrity_algorithms  = ["SHA2-256"]
+    phase2_lifetime_seconds      = 3600
+  }
+}
+
 resource "aws_vpn_connection" "rk24-nrt" {
   vpn_gateway_id      = aws_vpn_gateway.main.id
   customer_gateway_id = aws_customer_gateway.rk24-nrt.id
@@ -7,6 +20,26 @@ resource "aws_vpn_connection" "rk24-nrt" {
   tunnel1_ike_versions = ["ikev2"]
   tunnel2_ike_versions = ["ikev2"]
 
+  tunnel1_inside_cidr = "169.254.22.60/30"
+  tunnel2_inside_cidr = "169.254.22.64/30"
+
+  tunnel1_phase1_dh_group_numbers      = local.vpn_ikev2_options.phase1_dh_group_numbers
+  tunnel1_phase1_encryption_algorithms = local.vpn_ikev2_options.phase1_encryption_algorithms
+  tunnel1_phase1_integrity_algorithms  = local.vpn_ikev2_options.phase1_integrity_algorithms
+  tunnel1_phase1_lifetime_seconds      = local.vpn_ikev2_options.phase1_lifetime_seconds
+  tunnel1_phase2_dh_group_numbers      = local.vpn_ikev2_options.phase2_dh_group_numbers
+  tunnel1_phase2_encryption_algorithms = local.vpn_ikev2_options.phase2_encryption_algorithms
+  tunnel1_phase2_integrity_algorithms  = local.vpn_ikev2_options.phase2_integrity_algorithms
+  tunnel1_phase2_lifetime_seconds      = local.vpn_ikev2_options.phase2_lifetime_seconds
+  tunnel2_phase1_dh_group_numbers      = local.vpn_ikev2_options.phase1_dh_group_numbers
+  tunnel2_phase1_encryption_algorithms = local.vpn_ikev2_options.phase1_encryption_algorithms
+  tunnel2_phase1_integrity_algorithms  = local.vpn_ikev2_options.phase1_integrity_algorithms
+  tunnel2_phase1_lifetime_seconds      = local.vpn_ikev2_options.phase1_lifetime_seconds
+  tunnel2_phase2_dh_group_numbers      = local.vpn_ikev2_options.phase2_dh_group_numbers
+  tunnel2_phase2_encryption_algorithms = local.vpn_ikev2_options.phase2_encryption_algorithms
+  tunnel2_phase2_integrity_algorithms  = local.vpn_ikev2_options.phase2_integrity_algorithms
+  tunnel2_phase2_lifetime_seconds      = local.vpn_ikev2_options.phase2_lifetime_seconds
+
   tags = {
     Name      = "rk24-nrt"
     Project   = "rk24net"
@@ -15,7 +48,7 @@ resource "aws_vpn_connection" "rk24-nrt" {
 }
 
 resource "aws_customer_gateway" "rk24-nrt" {
-  bgp_asn    = 65002
+  bgp_asn    = 65152
   ip_address = "117.102.186.171"
   type       = "ipsec.1"
 
@@ -23,5 +56,9 @@ resource "aws_customer_gateway" "rk24-nrt" {
     Name      = "rk24-nrt"
     Project   = "rk24net"
     Component = "core/vpc"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
