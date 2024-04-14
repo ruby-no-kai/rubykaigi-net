e7ec6a45ef180b75421db262ea24bf383d5b752e
Kasumi Hanazuki <kasumi@rollingapple.net>
2024-04-14 13:26:33 +0000

    prom: Upgrade kube-prometheus-stack

--- tf/prom/kube-prometheus-stack.tf
+++ tf/prom/kube-prometheus-stack.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "kube-prometheus-stack" {
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
-  version    = "57.2.0"
+  version    = "58.1.1"
 
   name = "kube-prometheus-stack"
 
