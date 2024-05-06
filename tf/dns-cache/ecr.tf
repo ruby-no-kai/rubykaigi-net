b399a55b1cb43fdc79b72aa13092a8d405aeac00
Kasumi Hanazuki <kasumi@rollingapple.net>
2024-05-06 16:00:38 +0000

    ecr: dnscollector repo

--- tf/dns-cache/ecr.tf
+++ tf/dns-cache/ecr.tf
@@ -21,3 +21,28 @@ resource "aws_ecr_lifecycle_policy" "unbound" {
     ]
   })
 }
+
+
+resource "aws_ecr_repository" "dnscollector" {
+  name = "dnscollector"
+}
+
+resource "aws_ecr_lifecycle_policy" "dnscollector" {
+  repository = aws_ecr_repository.dnscollector.name
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 10
+        description  = "expire old images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
