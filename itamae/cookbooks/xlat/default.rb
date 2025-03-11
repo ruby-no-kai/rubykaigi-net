4f74a12715c6a929b4f4d85364fb06277a4aaa5b
Kasumi Hanazuki <kasumi@rollingapple.net>
2025-03-11 17:55:48 +0000

    xlat: Deploy xlat using clean Gemfile
    Gemfile in the xlat repository is for development.

--- itamae/cookbooks/xlat/default.rb
+++ itamae/cookbooks/xlat/default.rb
@@ -1,22 +1,27 @@
 include_cookbook 'ruby'
 package 'git'
 
+# migration
+execute 'rm -rf /opt/xlat' do
+  only_if 'test -e /opt/xlat/.git'
+end
+
 directory '/opt/xlat' do
   owner 'root'
   group 'root'
   mode '0755'
 end
 
-execute 'git clone xlat' do
-  command %{git clone https://github.com/sorah/xlat /opt/xlat}
-  not_if 'test -e /opt/xlat/.git'
+remote_file '/opt/xlat/Gemfile' do
+  owner 'root'
+  group 'root'
+  mode '0644'
 end
 
-execute 'git pull xlat' do
-  git = 'git -C /opt/xlat'
-
-  command %{#{git} reset --hard origin/main}
-  only_if %{#{git} fetch origin && test "$(#{git} rev-parse HEAD)" != "$(#{git} rev-parse origin/main)"}
+remote_file '/opt/xlat/Gemfile.lock' do
+  owner 'root'
+  group 'root'
+  mode '0644'
 end
 
 execute 'bundle install' do
@@ -25,10 +30,9 @@ execute 'bundle install' do
 set -e
 bundle config set --local deployment true
 bundle install
-(ruby -v && git rev-parse HEAD) | sha256sum >.bundle/hash
-bundle exec rake compile
+(ruby -v && cat Gemfile.lock) | sha256sum >.bundle/hash
 EOF
-  not_if '(ruby -v && git rev-parse HEAD) | sha256sum -c .bundle/hash'
+  not_if '(ruby -v && cat Gemfile.lock) | sha256sum -c .bundle/hash'
 end
 
 remote_file '/etc/systemd/system/xlat-siit.service' do
